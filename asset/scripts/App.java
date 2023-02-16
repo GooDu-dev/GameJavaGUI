a28b90504f62851c4ce6135289e4acc83181a48d
GooDu-dev <mokumokutv@gmail.com>
2023-02-17 02:21:41 +0700

    add csv file

--- asset/scripts/App.java
+++ asset/scripts/App.java
@@ -3,20 +3,37 @@ package asset.scripts;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class App {
+public class App implements Default{
+    // Frontend
     private static JFrame frame;
     private static final int APP_WIDTH=800, APP_HEIGHT=600;
-    private static String title;
+    private static String title="New Game";
+
+    // Backend
+    private String lastest_chapter, highest_score;
+    private String[] words;
+    private Player player;
+    private Enemy enemy;
 
     public App(){
-        title = "New Game";
+
     }
     public App(String title){
         this.title = title;
     }
     public void run(){
         createFrame();
+        loadSaved();
+        loadWord();
+        createEnv();
     }
     public void createFrame(){
         frame = new JFrame(title);
@@ -24,5 +41,34 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
         frame.setVisible(true);
+    }
+    public void loadSaved(){
+        try{
+            Scanner scan = new Scanner(new File(Default.DEFAULT_SAVED_PATH));
+            String _input = scan.nextLine();
+            System.out.println(_input);
+            String[] input = _input.split(", ");
+            System.out.println(Arrays.toString(input));
+            lastest_chapter = input[0];
+            highest_score = input[1];
+            scan.close();
+        }catch(FileNotFoundException e){
+            File f = new File(Default.DEFAULT_SAVED_PATH);
+            try{
+                FileWriter fw = new FileWriter(f);
+                fw.write("0");
+                fw.write("0");
+                fw.close();
+            }catch(IOException er){ return; }
+        }
+    }
+    public void loadWord(){
+        Scanner scan = new Scanner(Default.DEFAULT_WORD_PATH);
+        words = scan.nextLine().split(",");
+        scan.close();
+    }
+    public void createEnv(){
+        frame.getContentPane().setBackground(Color.black);
+
     }
 }
--- asset/scripts/Default.java
+++ asset/scripts/Default.java
@@ -2,4 +2,6 @@ package asset.scripts;
 
 public interface Default {
     public static final String DEFAULT_IMAGE_PATH = "../picture/default_image.png";
+    public static final String DEFAULT_SAVED_PATH = "../Saved/save.csv";
+    public static final String DEFAULT_WORD_PATH = "../Saved/word.csv";
 }
