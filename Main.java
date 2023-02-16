3e1fb413a234496756cb8f774e8dbb6f58f8818a
GooDu-dev <mokumokutv@gmail.com>
2023-02-17 01:37:57 +0700

    Head V.2

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import asset.scripts.App;
 public class Main{
     public static void main(String[] args) {
-        
+        App app = new App();
+        app.run();
     }
 }
--- asset/scripts/App.java
+++ asset/scripts/App.java
@@ -5,5 +5,24 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class App {
-    
+    private static JFrame frame;
+    private static final int APP_WIDTH=800, APP_HEIGHT=600;
+    private static String title;
+
+    public App(){
+        title = "New Game";
+    }
+    public App(String title){
+        this.title = title;
+    }
+    public void run(){
+        createFrame();
+    }
+    public void createFrame(){
+        frame = new JFrame(title);
+        frame.setSize(APP_WIDTH, APP_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
+    }
 }
