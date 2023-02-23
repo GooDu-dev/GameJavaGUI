3388b371924da7fc3fee0d226793062fdcf5409b
GooDu-dev <mokumokutv@gmail.com>
2023-02-23 21:22:48 +0700

    Add MainMenu, SelectEpisode, SelectChapter

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 import asset.scripts.App;
+import asset.scripts.Default;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
         App app = new App();
