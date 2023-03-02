e8393c740f40258dcac0fc53f9d0d096a5df62dd
Jay <Pakkhapol2444@gmail.com>
2023-03-02 21:54:27 +0700

    pdf

--- asset/scripts/Default.java
+++ asset/scripts/Default.java
@@ -7,5 +7,5 @@ import java.util.Scanner;
 public interface Default {
     public static final String DEFAULT_IMAGE_PATH = "./asset/picture/default_image.png";
     public static final String DEFAULT_SAVED_PATH = "./asset/Saved/save.txt";
-    public static final String DEFAULT_WORD_PATH = "./asset/Saved/word.txt";
+    public static final String DEFAULT_WORD_PATH = "./asset/Saved/words.json";
 }
---
+++ asset/scripts/WordMethods.java
@@ -0,0 +1,16 @@
+package asset.scripts;
+import java.io.File;
+import java.util.*;
+import java.io.*;
+public class WordMethods{
+    private ArrayList<String> words = new ArrayList<>();
+    private Integer currentLevel = 0;
+
+    private static void fetchWords(){
+        try {
+            Scanner scan = new Scanner(new File(Default.DEFAULT_WORD_PATH));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
