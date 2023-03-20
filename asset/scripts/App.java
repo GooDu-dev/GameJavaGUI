3cb7a57357b5053e52703c1e23f42b7f75302df7
achi <shishimaru4237@gmail.com>
2023-03-20 09:15:28 +0700

    no message

--- asset/scripts/App.java
+++ asset/scripts/App.java
@@ -49,6 +49,20 @@ public class App {
         frame.clearScreen();
         JLabel word = new JLabel(w);
         frame.getContentPane().add(word);
+
+        final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        final Runnable runnable = new Runnable() {
+            int countdownStarter = 1;
+            public void run() {
+                System.out.println(countdownStarter);
+                countdownStarter--;
+                if (countdownStarter < 0) {
+                    System.out.println("Timer Over!");
+                    scheduler.shutdown();
+                }
+            }
+        };
+        scheduler.scheduleAtFixedRate(runnable, 0, 1, SECONDS);
     }
     public static void chapterStage(String word){
         frame.clearScreen();
@@ -62,40 +76,45 @@ public class App {
         });
         frame.getContentPane().add(timer);
         timer.setFont(new Font(timer.getFont().getName(), timer.getFont().getStyle(), frame.getContentPane().getWidth()/50));
-        timer.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
+        timer.setAlignmentX(JLabel.LEFT_ALIGNMENT);
         t.start();
+        
+        ArrayList<Character> wordFromFetch = new ArrayList<>();
+        for(int i = 0; i < word.length(); i++){
+            wordFromFetch.add(word.charAt(i));
+        }
 
-        CustomPanel field = new CustomPanel();
-        frame.getContentPane().add(field);
-        field.setLayout(new BoxLayout(field, BoxLayout.X_AXIS));
-        field.setPanelSize(0.6, 0.5);
-        field.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+        // CustomPanel field = new CustomPanel();
+        // frame.getContentPane().add(field);
+        // field.setLayout(new BoxLayout(field, BoxLayout.X_AXIS));
+        // field.setPanelSize(0.6, 0.5);
+        // field.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 
         // create player
-        Player player = new Player(3, "", Type.ObjectType.Player);
-        field.add(player);
-        player.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+        // Player player = new Player(3, "", Type.ObjectType.Player);
+        // field.add(player);
+        // player.setAlignmentX(JLabel.LEFT_ALIGNMENT);
 
-        // create enemy
-        Enemy enemy = new Enemy(3, "", Type.ObjectType.Enemy);
-        field.add(enemy);
-        enemy.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
+        // // create enemy
+        // Enemy enemy = new Enemy(3, "", Type.ObjectType.Enemy);
+        // field.add(enemy);
+        // enemy.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
         
-        CustomPanel board = new CustomPanel();
-        frame.getContentPane().add(board);
-        board.setLayout(new BoxLayout(board, BoxLayout.Y_AXIS));
+        // CustomPanel board = new CustomPanel();
+        // frame.getContentPane().add(board);
+        // board.setLayout(new BoxLayout(board, BoxLayout.Y_AXIS));
         
-        //showWords(word);
-        CustomPanel h_container = new CustomPanel();
-        frame.getContentPane().add(h_container);
-        h_container.setLayout(new BoxLayout(h_container, BoxLayout.X_AXIS));
-        for(int i=0; i<player.getHp(); i++){
-            CustomLabel heart = new CustomLabel();
-            frame.getContentPane().add(heart);
-            if(i < player.getHp()-1){
-                h_container.add(Box.createRigidArea(new Dimension((int)(h_container.getWidth()*0.1), 0)));
-            }
-        }
+        // //showWords(word);
+        // CustomPanel h_container = new CustomPanel();
+        // frame.getContentPane().add(h_container);
+        // h_container.setLayout(new BoxLayout(h_container, BoxLayout.X_AXIS));
+        // for(int i=0; i<player.getHp(); i++){
+        //     CustomLabel heart = new CustomLabel();
+        //     frame.getContentPane().add(heart);
+        //     if(i < player.getHp()-1){
+        //         h_container.add(Box.createRigidArea(new Dimension((int)(h_container.getWidth()*0.1), 0)));
+        //     }
+        // }
         
     }
     public static void wordBeforeStart(){
@@ -122,70 +141,73 @@ public class App {
         };
         scheduler.scheduleAtFixedRate(runnable, 0, 1, SECONDS);
     }
-    
-    public static void showWords(String word) {
-        uniqueLetters = new HashSet<String>(Arrays.asList(word.split("")));
-        buttonMap = new HashMap<String, JButton>();
-        selectedLettersTextArea = new JTextArea(2, 20);
-    
-        // create an array of all letters A-Z
-        String[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-                "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-    
-        // add a random button for each letter in the alphabet
-        for(String letter : alphabet){
-            JButton charButton = new JButton(letter);
-            if(uniqueLetters.contains(letter)){
-                charButton.setBackground(Color.RED);
-            }
-            charButton.addActionListener(new ActionListener(){
-                @Override
-                public void actionPerformed(ActionEvent e){
-                    if(e.getSource() == charButton){
-                        if(uniqueLetters.contains(letter)){
-                            charButton.setBackground(Color.GREEN);
-                            removeLetter(letter);
-                            selectedLettersTextArea.append(letter);
-                            selectedLettersTextArea.append(" ");
-                            if(uniqueLetters.isEmpty()){
-                                JOptionPane.showMessageDialog(null, "Congratulations! You guessed the word " + word + "!");
-                            }
-                        }else{
-                            charButton.setBackground(Color.GRAY);
-                        }
-                    }
-                }
-            });
-            buttonMap.put(letter, charButton);
-        }
-    
-        // add a button to show the selected letters
-        JButton showSelectedButton = new JButton("Show Selected");
-        showSelectedButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Selected letters: " + selectedLettersTextArea.getText());
-            }
-        });
-    
-        // create a panel to hold the buttons and the "Show Selected" button
-        JPanel buttonPanel = new JPanel(new GridLayout(2, 13));
-        for (String letter : alphabet) {
-            buttonPanel.add(buttonMap.get(letter));
-        }
-        buttonPanel.add(showSelectedButton);
-    
-        // create a main panel to hold the button panel and the selected letters text area
-        JPanel mainPanel = new JPanel();
-        mainPanel.add(buttonPanel);
-        mainPanel.add(selectedLettersTextArea);
-    
-        // add the main panel to the frame and display it
-        frame.add(mainPanel);
-        frame.pack();
-        frame.setVisible(true);
-    }    
-    public static void removeLetter(String letter) {
-        uniqueLetters.remove(letter);
+    public static boolean checkWord(String a, String b){
+        return a.toUpperCase().equals(b.toUpperCase()) ? true : false;
     }
+    
+    // public static void showWords(String word) {
+    //     uniqueLetters = new HashSet<String>(Arrays.asList(word.split("")));
+    //     buttonMap = new HashMap<String, JButton>();
+    //     selectedLettersTextArea = new JTextArea(2, 20);
+    
+    //     // create an array of all letters A-Z
+    //     String[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+    //             "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+    
+    //     // add a random button for each letter in the alphabet
+    //     for(String letter : alphabet){
+    //         JButton charButton = new JButton(letter);
+    //         if(uniqueLetters.contains(letter)){
+    //             charButton.setBackground(Color.RED);
+    //         }
+    //         charButton.addActionListener(new ActionListener(){
+    //             @Override
+    //             public void actionPerformed(ActionEvent e){
+    //                 if(e.getSource() == charButton){
+    //                     if(uniqueLetters.contains(letter)){
+    //                         charButton.setBackground(Color.GREEN);
+    //                         removeLetter(letter);
+    //                         selectedLettersTextArea.append(letter);
+    //                         selectedLettersTextArea.append(" ");
+    //                         if(uniqueLetters.isEmpty()){
+    //                             JOptionPane.showMessageDialog(null, "Congratulations! You guessed the word " + word + "!");
+    //                         }
+    //                     }else{
+    //                         charButton.setBackground(Color.GRAY);
+    //                     }
+    //                 }
+    //             }
+    //         });
+    //         buttonMap.put(letter, charButton);
+    //     }
+    
+    //     // add a button to show the selected letters
+    //     JButton showSelectedButton = new JButton("Show Selected");
+    //     showSelectedButton.addActionListener(new ActionListener() {
+    //         @Override
+    //         public void actionPerformed(ActionEvent e) {
+    //             JOptionPane.showMessageDialog(null, "Selected letters: " + selectedLettersTextArea.getText());
+    //         }
+    //     });
+    
+    //     // create a panel to hold the buttons and the "Show Selected" button
+    //     JPanel buttonPanel = new JPanel(new GridLayout(2, 13));
+    //     for (String letter : alphabet) {
+    //         buttonPanel.add(buttonMap.get(letter));
+    //     }
+    //     buttonPanel.add(showSelectedButton);
+    
+    //     // create a main panel to hold the button panel and the selected letters text area
+    //     JPanel mainPanel = new JPanel();
+    //     mainPanel.add(buttonPanel);
+    //     mainPanel.add(selectedLettersTextArea);
+    
+    //     // add the main panel to the frame and display it
+    //     frame.add(mainPanel);
+    //     frame.pack();
+    //     frame.setVisible(true);
+    // }    
+    // public static void removeLetter(String letter) {
+    //     uniqueLetters.remove(letter);
+    // }
 }
